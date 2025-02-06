b5ff2ad5f394a6307174d398feb7d8448dd20e1a
alexeygalt <alexeygalt@gmail.com>
2025-02-06 13:19:53 +0300

    test

--- db/migrations/env.py
+++ db/migrations/env.py
@@ -61,6 +61,9 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    print('-----------------------------')
+    print(DATABASE_URL)
+    print('-----------------------------')
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
