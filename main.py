9ff2542cd58cc330da353b34db90108df5a35ef1
alexeygalt <alexeygalt@gmail.com>
2025-02-08 16:32:19 +0300

    cron to testing changed

--- main.py
+++ main.py
@@ -27,9 +27,13 @@ async def on_startup(dp):
 
     scheduler = AsyncIOScheduler()
 
-    scheduler.add_job(run_parsing, CronTrigger(hour="0,18", minute="0"))
-    scheduler.add_job(check_and_send_messages, CronTrigger(hour="8,12,16,20", minute="0"), args=[game_dao, bot])
-    scheduler.add_job(update_game_states, CronTrigger(minute="0"))
+    # scheduler.add_job(run_parsing, CronTrigger(hour="0,18", minute="0"))
+    # scheduler.add_job(check_and_send_messages, CronTrigger(hour="8,12,16,20", minute="0"), args=[game_dao, bot])
+    # scheduler.add_job(update_game_states, CronTrigger(minute="0"))
+
+    scheduler.add_job(run_parsing, CronTrigger(minute="15,45"))
+    scheduler.add_job(check_and_send_messages, CronTrigger(minute="5,35"), args=[game_dao, bot])
+    scheduler.add_job(update_game_states, CronTrigger(minute="20,50"))
     scheduler.start()
 
     await dp.start_polling(bot)
