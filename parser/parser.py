61178b645628ade3076b54233e0e77f4fde1a817
alexeygalt <alexeygalt@gmail.com>
2025-04-22 21:55:53 +0300

    fix empty active_game_data

--- parser/parser.py
+++ parser/parser.py
@@ -234,9 +234,9 @@ async def parsing_active_games() -> None:
         active_game_data = []
         for url, game_type in ACTIVE_GAMES_URLS:
             game_data = await fetch_and_parse_games(session, url, game_type)
-            if not game_data:
-                parser_logger.info("Получен пустой результат для игры из ACTIVE_GAMES_URLS. Откатываем изменения")
-                return
+            # if not game_data:
+            #     parser_logger.info("Получен пустой результат для игры из ACTIVE_GAMES_URLS. Откатываем изменения")
+            #     return
             active_game_data.extend(game_data)
 
         await gather_additional_game_data(session, active_game_data)
